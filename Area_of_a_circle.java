cf7648a4103ccbe1396e7a9e1419f7f375748d14
anithaachanta <119720845+anithaachanta@users.noreply.github.com>
2023-05-17 15:14:46 +0530

    import java.util.Scanner; public class sample {     public static void main(String args[])     {
        Scanner sc=new Scanner(System.in);
        
        float pi=3.14f,area;
        int r;
        r=sc.nextInt();
        area=pi*r*r;
        System.out.format("%.2f",area);
        sc.close();
        }
    }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class sample
+{
+    public static void main(String args[])
+    {
+        
+    
+    Scanner sc=new Scanner(System.in);
+    
+    float pi=3.14f,area;
+    int r;
+    r=sc.nextInt();
+    area=pi*r*r;
+    System.out.format("%.2f",area);
+    sc.close();
+    }
+}
