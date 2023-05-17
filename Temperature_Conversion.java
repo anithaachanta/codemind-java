dba1d9f7cfa0279640b712b40cd74319569014fa
anithaachanta <119720845+anithaachanta@users.noreply.github.com>
2023-05-17 14:20:23 +0530

    import java.util.Scanner; public class sample {         public static void main(String args[])         {
                int c;
                float f;
                Scanner sc=new Scanner(System.in);
                c=sc.nextInt();
                f=32+(c*(float)9/5);
                System.out.format("%.2f",f);
                sc.close();
            }
    }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class sample
+{
+        public static void main(String args[])
+        {
+            
+        
+            int c;
+            float f;
+            Scanner sc=new Scanner(System.in);
+            c=sc.nextInt();
+            f=32+(c*(float)9/5);
+            System.out.format("%.2f",f);
+            sc.close();
+        }
+}
