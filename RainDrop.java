52eb60d959cfc7892a9deee6de2c5f6c9630cefd
anithaachanta <119720845+anithaachanta@users.noreply.github.com>
2023-05-18 23:14:05 +0530

    import java.util.Scanner; public class sample {     public static void main(String args[])     {         int a;         String Sound="";
            Scanner sc=new Scanner(System.in);
            a=sc.nextInt();
            if(a%3==0)
            Sound=Sound+"Pling";
            if(a%5==0)
            Sound=Sound+"Plang";
            if(a%7==0)
            Sound=Sound+"Plong";
            if(Sound.isEmpty())
            System.out.println(a);
            else
            System.out.println(Sound);
            sc.close();
            
        }
    }

---
+++ RainDrop.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class sample
+{
+    public static void main(String args[])
+    {
+        int a;
+        String Sound="";
+        
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        if(a%3==0)
+        Sound=Sound+"Pling";
+        if(a%5==0)
+        Sound=Sound+"Plang";
+        if(a%7==0)
+        Sound=Sound+"Plong";
+        if(Sound.isEmpty())
+        System.out.println(a);
+        else
+        System.out.println(Sound);
+        sc.close();
+        
+    }
+}
