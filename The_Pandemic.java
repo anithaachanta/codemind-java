52d34ece5cafbf1d980a59392d2febdffd988af6
anithaachanta <119720845+anithaachanta@users.noreply.github.com>
2023-05-18 22:53:53 +0530

    import java.util.Scanner; public class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();
            if(n<3)
            System.out.println(n-1);
            else
            System.out.println("2");
            sc.close();
        }
    }

---
+++ The_Pandemic.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+
+        if(n<3)
+        System.out.println(n-1);
+        else
+        System.out.println("2");
+        sc.close();
+    }
+}
